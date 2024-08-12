ccb50f73443ecd145042e25dcdd6c80e6bc0ab22
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-11 21:35:21 -0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
